c99b129728f6e53ba46b3c070e9bb0577ad7c6be
camila <mariacamilaluango@gmail.com>
2025-02-19 21:19:08 -0500

    archivo pdf

--- insertionSort/script_i_titulo.js
+++ insertionSort/script_i_titulo.js
@@ -16,7 +16,11 @@ const libros = [
     { titulo: "Algoritmos Avanzados", anio: 2018, reservas: 120 },
     { titulo: "Introducción a JavaScript", anio: 2020, reservas: 90 },
     { titulo: "Estructuras de Datos", anio: 2015, reservas: 60 },
-    { titulo: "Sistemas Operativos", anio: 2019, reservas: 110 }
+    { titulo: "Sistemas Operativos", anio: 2019, reservas: 110 },
+    { titulo: "Teoria de sistemas", anio: 2018, reservas: 12 },
+    { titulo: "Hipnos", anio: 2013, reservas: 20 },
+    { titulo: "100 años de soledad", anio: 2012, reservas: 70 },
+    { titulo: "Español", anio: 2010, reservas: 11 }
 ]
 
 console.log(insertionSort(libros))
