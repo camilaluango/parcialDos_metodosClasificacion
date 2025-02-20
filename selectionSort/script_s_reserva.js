adf56fdaba61e86a10b7288d91b8c2c4fda162fb
camila <mariacamilaluango@gmail.com>
2025-02-19 19:47:39 -0500

    time seleccion por reserva

---
+++ selectionSort/script_s_reserva.js
@@ -0,0 +1,26 @@
+function seleccionSort(arr){
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++){
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++){
+            if (arr[j].reservas < arr[minIndex].reservas){
+                minIndex = j
+            }
+        }
+        let temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    } return arr;
+} 
+const libros = [
+    { titulo: "Algoritmos Avanzados", anio: 2018, reservas: 120 },
+    { titulo: "Introducción a JavaScript", anio: 2020, reservas: 90 },
+    { titulo: "Estructuras de Datos", anio: 2015, reservas: 60 },
+    { titulo: "Sistemas Operativos", anio: 2019, reservas: 110 }
+];
+
+console.log(seleccionSort(libros))
+
+console.time("seleccion por reserva");
+const librosOrdenados = seleccionSort(libros);
+console.timeEnd("seleccion por reserva");
