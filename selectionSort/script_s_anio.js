e9d3bcaa929196dcc2f61642db4142364bfb0e43
camila <mariacamilaluango@gmail.com>
2025-02-19 19:44:51 -0500

    organizar por año

--- selectionSort/script_s_anio.js
+++ selectionSort/script_s_anio.js
@@ -12,3 +12,14 @@ function seleccionSort(arr){
         arr[i] = temp;
     } return arr;
 } 
+const libros = [
+    { titulo: "Algoritmos Avanzados", anio: 2018, reservas: 120 },
+    { titulo: "Introducción a JavaScript", anio: 2020, reservas: 90 },
+    { titulo: "Estructuras de Datos", anio: 2015, reservas: 60 },
+    { titulo: "Sistemas Operativos", anio: 2019, reservas: 110 }
+];
+
+console.log(seleccionSort(libros))
+console.time("seleccion por año");
+const librosOrdenados = seleccionSort(libros);
+console.timeEnd("seleccion por año");
