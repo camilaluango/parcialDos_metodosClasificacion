3fc1dd2fb75ab720823125ea74ec5cc16d05530f
camila <mariacamilaluango@gmail.com>
2025-02-19 19:30:33 -0500

    time por titulo

---
+++ bubbleSort/script_b_titulo.js
@@ -0,0 +1,28 @@
+const bubbleSort = (array) => {
+    let swapped;
+    for (let i = array.length - 1; i > 0; i--){
+        swapped = false;
+
+        for (let j = 0; j < i; j++){
+            if (array[j].titulo > array[j + 1].titulo){
+                [array[j], array[j + 1]]=
+                [array[j + 1], array[j]];
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+    return array;
+}
+const libros = [
+    { titulo: "Algoritmos Avanzados", anio: 2018, reservas: 120 },
+    { titulo: "Introducción a JavaScript", anio: 2020, reservas: 90 },
+    { titulo: "Estructuras de Datos", anio: 2015, reservas: 60 },
+    { titulo: "Sistemas Operativos", anio: 2019, reservas: 110 }
+   ];
+
+console.log(bubbleSort(libros));
+
+console.time("bubbleSort por titulo");
+const librosOrdenados = bubbleSort(libros);
+console.timeEnd("bubbleSort por titulo")
