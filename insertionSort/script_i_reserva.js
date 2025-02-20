2fb59f23fde2b959aac1672a91d0dc6115ee393d
camila <mariacamilaluango@gmail.com>
2025-02-19 19:39:31 -0500

    time insertion por reserva

---
+++ insertionSort/script_i_reserva.js
@@ -0,0 +1,25 @@
+function insertionSort(arr){
+    let n = arr.length;
+    for (let i = 1; i < n; i++){
+        let key = arr[i].reservas;
+        let j = i -1;
+        while (j >= 0 && arr[j].anio > key){
+            arr[j + 1].reservas = arr[j].reservas;
+            j = j - 1
+        }
+        arr[j + 1].reservas = key; 
+    }
+    return arr;
+    
+}
+const libros = [
+    { titulo: "Algoritmos Avanzados", anio: 2018, reservas: 120 },
+    { titulo: "Introducción a JavaScript", anio: 2020, reservas: 90 },
+    { titulo: "Estructuras de Datos", anio: 2015, reservas: 60 },
+    { titulo: "Sistemas Operativos", anio: 2019, reservas: 110 }
+]
+
+console.log(insertionSort(libros))
+console.time("insercion por reservas");
+const librosOrdenados = insertionSort(libros);
+console.timeEnd("insercion por reservas");
